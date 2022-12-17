bb23c089f4db5dfb1210f01a0d74aa62e6a77bec
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2022-12-17 23:43:35 +0530

    windows click added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 console.log("script is runnin");
-document.querySelector(".windowslogo").addEventListener("click",()=>{
+
+document.querySelector(".windowsonclick").style.opacity="1";
+document.querySelector(".bottombar").addEventListener("click",()=>{
     console.log("clicked");
-    
+    if (document.querySelector(".windowsonclick img").style.bottom=="-45rem") {
+        document.querySelector(".windowsonclick img").style.bottom="4.5rem";
+
+        
+    } else {
+
+        document.querySelector(".windowsonclick img").style.bottom="-45rem";
+        
+    }
+   
     
 })
